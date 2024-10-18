ee9ed30e4c3c58ea7e97d52be0f61eb0dcabbd65
pjaguilar <pjaguilar@indra.es>
2024-10-18 11:19:54 +0200

    multiplicar dividir

---
+++ src/Dividir.java
@@ -0,0 +1,7 @@
+public class Dividir implements Operacion{
+
+    public double operar(double num1, double num2)
+    {
+        return num1/num2;
+    }
+}
---
+++ src/Operacion.java
@@ -0,0 +1,3 @@
+public interface Operacion {
+    public double operar(double num1,double num2);
+}
